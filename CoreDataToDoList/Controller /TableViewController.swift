6b43e9bb96c0022b124a50244aab45ee8083a40e
nikita_kogolenok <nikiton17ns@gmail.com>
2022-01-28 15:26:31 +0300

    Core Data stack Core Data Saving support

--- CoreDataToDoList/Controller /TableViewController.swift
+++ CoreDataToDoList/Controller /TableViewController.swift
@@ -6,11 +6,12 @@
 //
 
 import UIKit
+import CoreData
 
 class TableViewController: UITableViewController {
     
     // MARK: - Variable
-    var toDoList: [String] = []
+    var tasks: [Task] = []
     
     // MARK: - Life Cycle
     override func viewDidLoad() {
@@ -23,14 +24,37 @@ class TableViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return toDoList.count
+        return tasks.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        cell.textLabel?.text = toDoList[indexPath.row]
+        
+        let task = tasks[indexPath.row]
+        cell.textLabel?.text = task.title
         
         return cell
+    }
+    
+    // MARK: - Methods
+    private func saveTask(withTitle title: String) {
+        let context = getContext()
+        guard let entity = NSEntityDescription.entity(forEntityName: "Task", in: context) else { return }
+        
+        let taskObject = Task(entity: entity, insertInto: context)
+        taskObject.title = title
+        
+        do {
+            try context.save()
+            tasks.append(taskObject)
+        } catch let error as NSError {
+            print(error.localizedDescription)
+        }
+    }
+    
+    private func getContext() -> NSManagedObjectContext {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        return appDelegate.persistentContainer.viewContext
     }
     
     // MARK: - Action
@@ -38,8 +62,9 @@ class TableViewController: UITableViewController {
         let alertController = UIAlertController(title: "Add task", message: "Add new task", preferredStyle: .alert)
         
         let okAction = UIAlertAction(title: "OK", style: .default) { action in
-            if let textField = alertController.textFields?[0] {
-                self.toDoList.insert(textField.text ?? "Default", at: 0)
+            let textField = alertController.textFields?.first
+            if let newTaskTitle = textField?.text {
+                self.saveTask(withTitle: newTaskTitle)
                 self.tableView.reloadData()
             }
         }
